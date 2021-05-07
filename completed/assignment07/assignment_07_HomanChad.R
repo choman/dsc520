299a399ca27933b14d78108d0adb78685355eb7c
Chad Homan <choman@gmail.com>
2021-05-07 17:47:08 -0500

    Think I am good

--- completed/assignment07/assignment_07_HomanChad.R
+++ completed/assignment07/assignment_07_HomanChad.R
@@ -8,51 +8,61 @@ setwd(basedir)
 
 ## Load the `data/r4ds/heights.csv` to
 heights_df <- read.csv("data/r4ds/heights.csv")
-
+head(heights_df)
 # Fit a linear model
-earn_lm <-  lm(___ ~ ___ + ___ + ___ + ___ + ___, data=heights_df)
+earn_lm <-  lm(earn ~ age + height + sex + ed + age + race, data=heights_df)
 
 # View the summary of your model
 summary(earn_lm)
 
-predicted_df <- data.frame(
-  earn = predict(___, ___),
-  ed=___, race=___, height=___,
-  age=___, sex=___
+head(heights_df)
+
+predicted_df <- data.frame(earn = predict(earn_lm, heights_df),
+                           ed=heights_df$ed, 
+                           race=heights_df$race, 
+                           height=heights_df$height,
+                           age=heights_df$age, 
+                           sex=heights_df$sex
   )
 
+head(predicted_df)
+
 ## Compute deviation (i.e. residuals)
-mean_earn <- ___
+mean_earn <- mean(heights_df$earn)
 ## Corrected Sum of Squares Total
-sst <- ___
+sst <- sum((mean_earn - heights_df$earn)^2)
 ## Corrected Sum of Squares for Model
-ssm <- ___
+ssm <- sum((mean_earn - predicted_df$earn)^2)
 ## Residuals
-residuals <- ___
+residuals <- heights_df$earn - predicted_df$earn
 ## Sum of Squares for Error
-sse <- ___
+sse <- sum(residuals^2)
 ## R Squared
-r_squared <- ___
+r_squared <- ssm / sst
+r_squared
 
 ## Number of observations
-n <- ___
+n <- prod(dim(heights_df))
+n
 ## Number of regression paramaters
 p <- 8
 ## Corrected Degrees of Freedom for Model
-dfm <- ___
+dfm <- p - 1
 ## Degrees of Freedom for Error
-dfe <- ___
+dfe <- n - p
 ## Corrected Degrees of Freedom Total:   DFT = n - 1
-dft <- ___
+dft <- n - 1
 
 ## Mean of Squares for Model:   MSM = SSM / DFM
-msm <- ___
+msm <- ssm / dfm
 ## Mean of Squares for Error:   MSE = SSE / DFE
-mse <- ___
+mse <- sse / dfe
 ## Mean of Squares Total:   MST = SST / DFT
-mst <- ___
+mst <- sst / dft
 ## F Statistic
-f_score <- ___
+f_score <- msm/mse
+f_score
 
 ## Adjusted R Squared R2 = 1 - (1 - R2)(n - 1) / (n - p)
-adjusted_r_squared <- ___
+adjusted_r_squared <- 1 - (1 - r_squared) * (n - 1) / (n - p)
+adjusted_r_squared
